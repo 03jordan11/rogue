0716269329f335dbacefdad8b021b28e01721c97
Eve <jordanburnes@gmail.com>
2018-03-29 10:05:20 -0500

    Changed battle function to return a victor, added a roll function which takes a k-sided dice as an argument and returns a random number from 1 to that side amount. Also added function enemyEncounter(), which does nothing at the time, but is going to handle fights entirely such as fighting, blocking(adding later), etc. I will make another xEncounter() function to deal things like drinking potions or resting. I may create an Encounter class that just contains functions for these things to clean up the code.

--- game/main.cpp
+++ game/main.cpp
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <math.h>
 #include <string.h>
 #include "Character.cpp"
+#include <time.h>
 
 using namespace std;
 
-Character battle(Character* player, Character* enemy);
+void wait(int seconds);
+int roll(int sides);
+int battle(Character* player, Character* enemy);	//1 is player victory, 0 is enemy
+void enemyEncounter(Character* player);
+Character getEnemy(int tile);
 
 int main(){
 	Character ted("ted", 100, 5, 5, 10);
@@ -14,14 +20,40 @@ int main(){
 	battle(&ted, &bob);
 	printf("The character's name is %s. Below are his stats:\n", ted.getName().c_str());
 	printf("Health: %d\nAttack: %d\nDefense: %d\nSpeed: %d\n", ted.getHealth(), ted.getAttPower(), ted.getDefPower(), ted.getSpeed());
+	for(int i = 0; i < 20; i++){
+		printf("\n%d", roll(100));
+		wait(1);
+	}
 	return 0;
 }
 
-Character battle(Character* player, Character* enemy){
+int battle(Character* player, Character* enemy){
+	
 	if (player->getAttPower() >= enemy->getDefPower()){
 		enemy->takeDamage(player->getAttPower());
 	}
 	if(player->getDefPower() <= enemy->getAttPower()){
 		player->takeDamage(enemy->getAttPower());
 	}
+}
+
+void enemyEncounter(Character* player){
+	
+}
+
+Character getEnemy(){
+	//PULL RANDOM ENEMY FROM TEXT FILE HERE
+	Character enemy("goblin", 50, 3, 2, 5);
+	return enemy;
+}
+
+int roll(int sides){
+	srand(time(NULL));		//IS IT BETTER TO SEED IN MAIN?
+	return rand() % sides + 1;
+}
+
+void wait(int seconds){
+	clock_t endwait; 
+	endwait = clock () + seconds * CLOCKS_PER_SEC ; 
+	while (clock() < endwait) {}
 }
