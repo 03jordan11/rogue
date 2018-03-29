28d5ccefa908214cafd6207a9d5cbfc48dd39460
Eve <jordanburnes@gmail.com>
2018-03-29 10:36:37 -0500

    Filled in enemyEncounter() function. Havent fleshed it out much but it works at a basic level.

--- game/main.cpp
+++ game/main.cpp
@@ -17,13 +17,9 @@ Character getEnemy(int tile);
 int main(){
 	Character ted("ted", 100, 5, 5, 10);
 	Character bob("bob", 100, 5, 5, 10);
-	battle(&ted, &bob);
 	printf("The character's name is %s. Below are his stats:\n", ted.getName().c_str());
 	printf("Health: %d\nAttack: %d\nDefense: %d\nSpeed: %d\n", ted.getHealth(), ted.getAttPower(), ted.getDefPower(), ted.getSpeed());
-	for(int i = 0; i < 20; i++){
-		printf("\n%d", roll(100));
-		wait(1);
-	}
+	enemyEncounter(&ted);
 	return 0;
 }
 
@@ -38,12 +34,42 @@ int battle(Character* player, Character* enemy){
 }
 
 void enemyEncounter(Character* player){
+	Character enemy = getEnemy(1);
+	int answer = 1;
 	
+	printf("You have encountered a %s! ", enemy.getName().c_str());
+	while (answer != 0){
+		printf("How would you like to proceed?\n");
+		printf("1: fight\n2: run");										//will later add a talk option
+		cin >> answer;
+		if(answer == 1){
+			battle(player, &enemy);
+			if(player->getHealth() <= 0){
+				printf("You have fallen!\n");
+				exit(0);
+			}
+			else if(enemy.getHealth() <= 0){
+				printf("You killed it!\n");
+				break;
+			}
+			printf("\nYou have %d health left!", player->getHealth());
+		}
+		else if(answer == 2){
+			if(player->getSpeed() < enemy.getSpeed()){
+				printf("The creature if faster than you! You fail to escape and he swipes you for %d damage!", enemy.getAttPower());
+				player->takeDamage(enemy.getAttPower());
+			}
+			else{
+				printf("You have succesfully escaped!\n");
+				break;
+			}
+		}
+	}
 }
 
-Character getEnemy(){
+Character getEnemy(int tile){
 	//PULL RANDOM ENEMY FROM TEXT FILE HERE
-	Character enemy("goblin", 50, 3, 2, 5);
+	Character enemy("goblin", 50, 20, 2, 5);
 	return enemy;
 }
 
